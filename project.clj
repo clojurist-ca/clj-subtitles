d7bf692fba19bcc77585a006a84524b43031d08d
Robert Medeiros <robert@crimeminister.org>
2021-03-17 00:20:24 -0400

    chore: update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/clojurist-ca/clj-subtitles/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                  [org.clojure/tools.nrepl "0.2.12"]
-                                  [org.clojure/tools.trace "0.7.9"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.1.0"]
+                                  [org.clojure/tools.nrepl "0.2.13"]
+                                  [org.clojure/tools.trace "0.7.11"]]
                    :source-paths ["dev"]}}
   :min-lein-version "2.5.0")
